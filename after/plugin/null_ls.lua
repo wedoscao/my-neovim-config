239178adae1160b7deedc0829db2755d7c5c4f6c
wedoscao <hoaisang0806@gmail.com>
2023-08-23 17:38:51 +0700

    Add cmake, cpp, c++ formatter

--- after/plugin/null_ls.lua
+++ after/plugin/null_ls.lua
@@ -3,33 +3,33 @@ vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 
 require("mason").setup()
 require("mason-null-ls").setup({
-    ensure_installed = { "stylua", "prettierd", "sql-formatter", "rustfmt", "beautysh" },
-    automatic_installation = false,
-    handlers = {},
+	ensure_installed = { "stylua", "prettierd", "sql-formatter", "rustfmt", "beautysh", "cmakelang", "clang-format" },
+	automatic_installation = false,
+	handlers = {},
 })
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.prettierd.with({
-            env = {
-                PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/.prettierrc.json"),
-            },
-        }),
-    },
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
-                    vim.lsp.buf.format({ async = false })
-                end,
-            })
-        end
-    end,
+	sources = {
+		null_ls.builtins.formatting.prettierd.with({
+			env = {
+				PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/utils/.prettierrc.json"),
+			},
+		}),
+	},
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					-- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
 })
