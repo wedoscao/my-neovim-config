5ddecbb34f9de406fdc00c0dd66bc4218dfec033
wedoscao <hoaisang0806@gmail.com>
2023-09-07 18:53:41 +0700

    Change nvim-ts-rainbow2 to rainbow-delimiters.nvim

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -41,16 +41,6 @@ require("nvim-treesitter.configs").setup({
 		additional_vim_regex_highlighting = false,
 	},
 
-	rainbow = {
-		enable = true,
-		-- list of languages you want to disable the plugin for
-		disable = {},
-		-- Which query to use for finding delimiters
-		query = "rainbow-parens",
-		-- Highlight the entire buffer all at once
-		strategy = require("ts-rainbow").strategy.global,
-	},
-
 	autotag = {
 		enable = true,
 	},
--- lua/wedoscao/packer.lua
+++ lua/wedoscao/packer.lua
@@ -27,12 +27,12 @@ packer.startup(function(use)
 	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x", requires = { "nvim-lua/plenary.nvim" } })
 	use({
 		"nvim-treesitter/nvim-treesitter",
-		"HiPhish/nvim-ts-rainbow2",
 		"nvim-treesitter/nvim-treesitter-context",
 		"windwp/nvim-ts-autotag",
 		"nvim-treesitter/nvim-treesitter-refactor",
 		"nvim-treesitter/playground",
 	})
+	use("HiPhish/rainbow-delimiters.nvim")
 	use("mbbill/undotree")
 	use("tpope/vim-fugitive")
 	use("williamboman/mason.nvim")
